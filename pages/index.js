c0a66115b2097b0b62536cc4c01eb27f1e7b6760
Alena Katkova <katkova.web@gmail.com>
2022-06-16 23:32:35 +0200

    static rendering with data: from files and from pokemon API

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export default function Home({ allPostsData, pokemons }) {
+    console.log(pokemons)
     return (
         <Layout home>
             <Head>
@@ -15,6 +17,43 @@ export default function Home() {
                     <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
                 </p>
             </section>
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>Blog</h2>
+                <ul className={utilStyles.list}>
+                    {allPostsData.map(({ id, date, title }) => (
+                        <li className={utilStyles.listItem} key={id}>
+                            {title}
+                            <br />
+                            {id}
+                            <br />
+                            {date}
+                        </li>
+                    ))}
+                </ul>
+            </section>
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>And also some pokemons!</h2>
+                <ul className={utilStyles.list}>
+                    {pokemons.map(({ name }) => (
+                        <li className={utilStyles.listItem} key={name}>
+                            {name}
+                        </li>
+                    ))}
+                </ul>
+            </section>
         </Layout>
     );
+}
+
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData();
+    const pokemons = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0')
+        .then(response => response.json())
+        .then(data => data.results);
+    return {
+        props: {
+            allPostsData,
+            pokemons
+        },
+    };
 }
