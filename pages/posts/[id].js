76f689b735cc432bb1114cd427caaf7a2b5372b2
Alena Katkova <katkova.web@gmail.com>
2022-06-17 17:46:19 +0200

    show post content on the post page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,18 +1,26 @@
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
 
 
 export default function Post({ postData }) {
     return (
         <Layout>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+
             {postData.title}
             <br />
             {postData.id}
             <br />
             {postData.date}
+            <br />
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
         </Layout>
     );
 }
+
 export async function getStaticPaths() {
     const paths = getAllPostIds();
     return {
@@ -22,7 +30,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-    const postData = getPostData(params.id);
+    const postData = await getPostData(params.id);
     return {
         props: {
             postData,
